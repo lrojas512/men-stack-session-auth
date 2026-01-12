5f075ed7334d219177653fe6f6a61eda90e9a3ac
Luis Rojas <lmrojasvilla@gmail.com>
2026-01-12 14:38:57 -0500

    debugging updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,59 +1,64 @@
-const express= require('express')
-const router= express.Router()
-const User = require('../models/user.js')
-const bcrypt =require('bcrypt')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
+});
 
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in.ejs");
+});
 
-router.get('/sign-up',(req,res)=>{
-res.render('auth/sign-in.ejs')
-})
+router.get("/sign-out", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+});
 
-router.get('/sign-out',(req,res)=>{
-    req.session.destroy() 
-    res.redirect('/')
-})
+router.post("/sign-in", async (req, res) => {
+  const userInDB = await User.findOne({ username: req.body.username });
+  if (!userInDB) {
+    return res.send(
+      `A user with username ${req.body.username} does not exist.`
+    );
+  }
 
-router.post('/sign-in', async (req,res)=>{
-     const userInDB = await User.findOne({ username: req.body.username})
-    if(!userInDB){
-        return res.send(`A user with username ${req.body.username} does not exist.`)
-    }
+  const isValidPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDB.password
+  );
+  if (!isValidPassword) {
+    return res.send(`Password Incorrect`);
+  }
 
-const isValidPassword = bcrypt.compareSync(req.body.password,userInDB.password)
-if(!isValidPassword){
-    return res.send(`Password incorrect`)
-}
-
-req.session.user = {
+  req.session.user = {
     username: userInDB.username,
     _id: userInDB._id,
-}
+  };
 
-// req.session.save(()=>)
+  req.session.save(() => {
+    res.redirect("/");
+  });
+});
 
-res.redirect('/')
+router.post("/sign-up", async (req, res) => {
+  const userInDB = await User.findOne({ username: req.body.username });
+  if (userInDB) {
+    return res.send(`A user with username ${req.body.username} already exist.`);
+  }
 
-})
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send(`Your password and password confirm must match!`);
+  }
 
-router.post('/sign-up', async (req,res)=>{
-    const userInDB = await User.findOne({ username: req.body.username})
-    if(userInDB){
-        return res.send(`A user with username ${req.body.username} already exists`)
-    }
-    if(req.body.password !== req.body.confirmPassword){
-        return res.send(`Your password and password confrim must match!`)
-    }
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
 
+  const newUser = await User.create(req.body);
 
-    const hashPassword =bcrypt.hashSync(req.body.password,10)
+  res.send(newUser);
+});
 
-    req.body.password = hashPassword
-
-    const newUser = await User.Create(req.body)
-
-    res.send(newUser)
-
-
-})
-module.exports = router
+module.exports = router;
